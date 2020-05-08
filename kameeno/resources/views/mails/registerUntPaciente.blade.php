b9f94a644abbf59d0cdb73e2d2372989ecb577e5
Javier9818 <53130037+Javier9818@users.noreply.github.com>
2020-05-08 00:43:43 -0500

    cambios

--- kameeno/resources/views/mails/registerUntPaciente.blade.php
+++ kameeno/resources/views/mails/registerUntPaciente.blade.php
@@ -8,7 +8,13 @@
 </head>
 <body>
     <h1>UNT A TUS PACIENTES</h1>
-    <h3>Hola {{$names}}, muchas gracias por haber completado la primera etapa de registro y validación, a continuación deberá de llenar un formulario con sus datos personales, datos que serán cifrados por motivos de seguridad y discresión.</h3>
-    <h4>Para acceder al <a href="{{$url}}">formulario</a> solo tiene que dar <a href="{{$url}}">click</a> en el siguiente enlace: <a href="{{$url}}">ENLACE DE FORMULARIO</a></h4>
+    <h3>Hola {{$names}}</h3>
+    <p>Muchas gracias por haber completado la primera etapa de registro y validación, a continuación deberá seguir los siguientes pasos: </p>
+    <ol>
+        <li>Llenar el formulario de registro con sus datos personales dando click en el siguiente enlace: <a href="{{$url}}">ENLACE DE FORMULARIO</a> </li>
+        <li>Imprima y firme el consentimiento informado, y reenvie a este email una foto del mismo: <a href="https://drive.google.com/open?id=1katA7ffu8av2UagLzEmtK46QTfCUi15p">Consentimiento informado</a></li>
+
+    </ol>
+
 </body>
 </html>
